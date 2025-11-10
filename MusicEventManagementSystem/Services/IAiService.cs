28e72e56c25fad8853df2bc53c13d15e90d11a33
KindaRusty <118987288+KindaRusty@users.noreply.github.com>
2025-11-10 10:46:53 +0700

    v1.1.2

--- MusicEventManagementSystem/Services/IAiService.cs
+++ MusicEventManagementSystem/Services/IAiService.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic; // THÊM
+using System.Collections.Generic;
 using System.Threading.Tasks;
-using MusicEventManagementSystem.Models; // THÊM
+using MusicEventManagementSystem.Models;
 
 namespace MusicEventManagementSystem.Services
 {
